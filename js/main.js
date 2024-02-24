42e647c1a5abf8b09f55ebb9c29cef4140cae4af
jasminechicoine <113364494+jasminechicoine@users.noreply.github.com>
2024-02-23 19:05:19 -0500

    edited homepage by adding name and hero image and fixed some other things

--- js/main.js
+++ js/main.js
@@ -1,14 +1,45 @@
 (() => {
+    const tl = gsap.timeline();
 
-const tl = gsap.timeline();
+    gsap.fromTo("#prod-img-1", { x: "-100%" }, { x: "0%", duration: 0.7, scrollTrigger: {
+        trigger: "#sec-1",
+        start: "top center",
+        end: "center center",
+        scrub: true
+    }});
+    
+    gsap.fromTo("#text-1", { x: "100%" }, { x: "0%", duration: 0.7, delay: 0, scrollTrigger: {
+        trigger: "#sec-1",
+        start: "top center",
+        end: "center center",
+        scrub: true
+    }});
 
- gsap.fromTo("#prod-img-1", { x: "-100%" }, { x: "0%", duration: 0.7 });
- gsap.fromTo("#text-1", { x: "100%" }, { x: "0%", duration: 0.7, delay: 0 });
+    gsap.fromTo("#prod-img-2", { x: "100%" }, { x: "0%", duration: 0.7, scrollTrigger: {
+        trigger: "#sec-2",
+        start: "top center",
+        end: "center center",
+        scrub: true
+    }});
+    
+    gsap.fromTo("#text-2", { x: "-100%" }, { x: "0%", duration: 0.7, delay: 0, scrollTrigger: {
+        trigger: "#sec-2",
+        start: "top center",
+        end: "center center",
+        scrub: true
+    }});
 
- gsap.fromTo("#prod-img-2", { x: "100%" }, { x: "0%", duration: 0.7 });
- gsap.fromTo("#text-2", { x: "-100%" }, { x: "0%", duration: 0.7, delay: 0 });
-
- gsap.fromTo("#prod-img-3", { x: "-100%" }, { x: "0%", duration: 0.7 });
- gsap.fromTo("#text-3", { x: "100%" }, { x: "0%", duration: 0.7, delay: 0 });
-
+    gsap.fromTo("#prod-img-3", { x: "-100%" }, { x: "0%", duration: 0.7, scrollTrigger: {
+        trigger: "#sec-3",
+        start: "top center",
+        end: "center center",
+        scrub: true
+    }});
+    
+    gsap.fromTo("#text-3", { x: "100%" }, { x: "0%", duration: 0.7, delay: 0, scrollTrigger: {
+        trigger: "#sec-3",
+        start: "top center",
+        end: "center center",
+        scrub: true
+    }});
 })();
